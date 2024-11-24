b27e7e680d0339c20092780cfa7ab7b8cbb5c6aa
Carol <carolduarte2000@gmail.com>
2024-11-24 16:28:32 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-// Carregar Header e Footer
-async function loadHeaderAndFooter() {
-  try {
-    const headerResponse = await fetch("/includes/header.html");
-    const headerData = await headerResponse.text();
-    document.getElementById("header").innerHTML = headerData;
-
-    const footerResponse = await fetch("/includes/footer.html");
-    const footerData = await footerResponse.text();
-    document.getElementById("footer").innerHTML = footerData;
-  } catch (error) {
-    console.error("Erro ao carregar o conteúdo:", error);
-  }
-}
-
-loadHeaderAndFooter();
-
 // Adicionar item à sacola
 function addToCart(product) {
   let cart = JSON.parse(localStorage.getItem("cart")) || [];
